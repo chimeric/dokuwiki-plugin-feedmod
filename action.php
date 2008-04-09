b2bedcb07c3bd2d3ca99e3b1e0b3835cb0f67e92
Michael Klier <chi@chimeric.de>
2008-04-09 22:01:49 +0200

    initial import
    darcs-hash:20080409200149-23886-1cb19b2d877183affebada0543ca1905c44a1b77.gz

---
+++ action.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * DokuWiki Action Plugin Feedmod
+ * 
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Michael Klier <chi@chimeric.de>
+ */
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_INC.'inc/plugin.php');
+
+/**
+ * All DokuWiki plugins to extend the admin function
+ * need to inherit from this class
+ */
+class action_plugin_feedmod extends DokuWiki_Action_Plugin {
+
+    function getInfo() {
+        return array(
+                'author' => 'Michael Klier',
+                'email'  => 'chi@chimeric.de',
+                'date'   => '2008-04-09',
+                'name'   => 'feedmod',
+                'desc'   => 'Modifies feed items for nicer full html feeds.',
+                'url'    => 'http://www.chimeric.de/projects/dokuwiki/plugin/feedmod'
+            );
+    }
+
+    // register hook
+    function register(&$controller) {
+        $controller->register_hook('FEED_ITEM_ADD', 'BEFORE', $this, '_feedmod');
+    }
+
+    /**
+     * Removes the headline which is already set in the item title and adds a link
+     * to the discussion section if a discussion exists
+     *
+     * @author Michael Klier <chi@chimeric.de>
+     */
+    function _feedmod(&$event, $param) {
+        global $conf;
+        global $opt;    // options feed.php is called with
+
+        if($opt['item_content'] == 'html') {
+
+            // only act if were linking to the current version of the page
+            if($opt['link_to'] == 'current') {
+
+                $url = parse_url($event->data['item']->link);
+                $base_url = getBaseURL();
+
+                // determine page id by rewrite mode
+                switch($conf['userewrite']) {
+                    case 0:
+                        preg_match('#id=([^&]*)#', $url['query'], $match);
+                        $id = cleanID(str_replace($base_url, '', $match[1]));
+                        break;
+                    case 1:
+                        $id = cleanID(str_replace('/',':',str_replace($base_url, '', $url['path'])));
+                        break;
+                    case 2:
+                        preg_match('#doku.php/([^&]*)#', $url['path'], $match);
+                        $id = cleanID(str_replace($base_url, '', $match[1]));
+                        break;
+                }
+
+                // strip first heading and replace item title
+                $firstheading = p_get_metadata($id, 'title', false);
+                $event->data['item']->description = preg_replace('#.*' . $firstheading . '.*\n#', '', $event->data['item']->description);
+                $event->data['item']->title = $firstheading;
+
+                // check for discussion file
+                if(@file_exists(metaFN($id, '.comments'))) {
+                    $clink = '<a href="' . $event->data['item']->link . '#discussion__section" title="'. $this->getLang('comments') . '">' . $this->getLang('comments') . '</a>';
+                    $event->data['item']->description .= "<br />" . $clink;
+                }
+            } 
+        }
+    }
+}
+
+// vim:ts=4:sw=4:enc=utf-8:
---
+++ lang/en/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * English language file for DokuWiki action plugin Feedmod
+ * 
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author  Michael Klier <chi@chimeric.de>
+ */
+
+$lang['comments'] = 'Read or add comments to this article';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
+?>
